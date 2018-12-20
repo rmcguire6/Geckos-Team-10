aba4713a42152f788aa27d095cbdec328fdcbc92
ChauTran73 <cmtran7393@gmail.com>
2018-12-20 12:47:12 -0500

    Add dummy data

---
+++ data.js
@@ -0,0 +1,172 @@
+[
+    {
+        "id": 21,
+        "name": "Vice Bier",
+        "tagline": "Hoppy Wheat Bier.",
+        "first_brewed": "04/2013",
+        "description": "Our take on the classic German Kristallweizen. A clear German wheat beer, layers of bubblegum and vanilla perfectly balanced with the American and New Zealand hops.",
+        "image_url": "https://images.punkapi.com/v2/keg.png",
+        "abv": 4.3,
+        "ibu": 25,
+        "food_pairing": [
+            "Citrus salad with balsamic dressing",
+            "Lobster",
+            "Ginger and orange cookies"
+        ],
+    },
+    {
+        "id": 22,
+        "name": "Devine Rebel (w/ Mikkeller)",
+        "tagline": "Oak-aged Barley Wine.",
+        "first_brewed": "12/2008",
+        "description": "Two of Europe's most experimental, boundary-pushing brewers, BrewDog and Mikkeller, combined forces to produce a rebellious beer that combined their respective talents and brewing skills. The 12.5% Barley Wine fermented well, and the champagne yeast drew it ever closer to 12.5%. The beer was brewed with a single hop variety and was going to be partially aged in oak casks.",
+        "image_url": "https://images.punkapi.com/v2/22.png",
+        "abv": 12.5,
+        "ibu": 100,
+        "food_pairing": [
+            "Stilton on gingerbread biscuits",
+            "Pot roasted artichokes",
+            "Lamb chops with caramelised figs"
+        ],
+    },
+    {
+        "id": 23,
+        "name": "Storm",
+        "tagline": "Islay Whisky Aged IPA.",
+        "first_brewed": "12/2007",
+        "description": "Dark and powerful Islay magic infuses this tropical sensation of an IPA. Using the original Punk IPA as a base, we boosted the ABV to 8% giving it some extra backbone to stand up to the peated smoke imported directly from Islay.",
+        "image_url": "https://images.punkapi.com/v2/23.png",
+        "abv": 8,
+        "ibu": 60,
+        "food_pairing": [
+            "Oysters",
+            "Hickory smoked ham",
+            "Rocky Road"
+        ],
+    },
+    {
+        "id": 24,
+        "name": "The End Of History",
+        "tagline": "The World's Strongest Beer.",
+        "first_brewed": "06/2011",
+        "description": "The End of History: The name derives from the famous work of philosopher Francis Fukuyama, this is to beer what democracy is to history. Complexity defined. Floral, grapefruit, caramel and cloves are intensified by boozy heat.",
+        "image_url": "https://images.punkapi.com/v2/24.png",
+        "abv": 55,
+        "ibu": null,
+        "food_pairing": [
+            "Roasted wood pigeon with black pudding",
+            "Pan seared venison fillet with juniper sauce",
+            "Apricot coconut cake"
+        ],
+    },
+    {
+        "id": 25,
+        "name": "Bad Pixie",
+        "tagline": "Spiced Wheat Beer.",
+        "first_brewed": "10/2008",
+        "description": "2008 Prototype beer, a 4.7% wheat ale with crushed juniper berries and citrus peel.",
+        "image_url": "https://images.punkapi.com/v2/25.png",
+        "abv": 4.7,
+        "ibu": 45,
+        "food_pairing": [
+            "Poached sole fillet with capers",
+            "Summer fruit salad",
+            "Banana split"
+        ],
+      
+    },
+    {
+        "id": 26,
+        "name": "Skull Candy",
+        "tagline": "Pacific Hopped Amber Bitter.",
+        "first_brewed": "02/2010",
+        "description": "The first beer that we brewed on our newly commissioned 5000 litre brewhouse in Fraserburgh 2009. A beer with the malt and body of an English bitter, but the heart and soul of vibrant citrus US hops.",
+        "image_url": "https://images.punkapi.com/v2/keg.png",
+        "abv": 3.5,
+        "ibu": 33,
+        
+                      
+        "food_pairing": [
+            "Haggis bon bons",
+            "Rosemary and lemon roast chicken",
+            "Oatmeal and cranberry cookies"
+        ],
+    },
+    {
+        "id": 27,
+        "name": "Dog B",
+        "tagline": "Cacao, Coffee Chili Anniversary Stout.",
+        "first_brewed": "05/2013",
+        "description": "Our anniversary stout was based on AB:04, one of our most sought after beers ever. This beer is slightly different each year, with subtle variations coming into play purely down to environmental and fermentation factors. What doesn't change is the rich dark chocolate, bitter coffee and chilli warmth of this monumental Stout.",
+        "image_url": "https://images.punkapi.com/v2/27.png",
+        "abv": 15,
+        "ibu": 90,
+        "food_pairing": [
+            "Blackened cajun beef",
+            "Blue cheese",
+            "Flourless chocolate cheesecake"
+        ],
+    },
+    {
+        "id": 28,
+        "name": "HBC 369",
+        "tagline": "Single Hop India Pale Ale – 2012.",
+        "first_brewed": "02/2012",
+        "description": "HBC 369 brings rich fruity flavours with the tiniest layer of candy coating on top. HBC 369 (the hop) can bring notes of blueberries, pear, and possibly even sweet potato. HBC 369 (the beer) was balanced, with just one hop providing a complex aroma, and a dry bitterness.",
+        "image_url": "https://images.punkapi.com/v2/28.png",
+        "abv": 6.7,
+        "ibu": 67,
+        "food_pairing": [
+            "Spicy jalapeño & seared steak fajitas",
+            "Grilled spicy corn with feta crumbles",
+            "Blueberry pie"
+        ],
+        
+    },
+    {
+        "id": 29,
+        "name": "10 Heads High",
+        "tagline": "Imperial Red Ale.",
+        "first_brewed": "04/2013",
+        "description": "10 Heads High is loosely based on our awesome 2011 Prototype beer Hops Kill Nazis. This is an uncompromising 7.8% Imperial Red Ale loaded high with American Hops. Think of this as an Imperial India Red Ale, or a super-charged version of 5am Saint. Either way this is a seriously good beer!",
+        "image_url": "https://images.punkapi.com/v2/29.png",
+        "abv": 7.8,
+        "ibu": 70,
+        "food_pairing": [
+            "Arrabiata pasta",
+            "Mulled cheddar",
+            "Toasted almond cake"
+        ],
+    },
+    {
+        "id": 30,
+        "name": "Dana - IPA Is Dead",
+        "tagline": "Single Hop India Pale Ale.",
+        "first_brewed": "02/2013",
+        "description": "Hailing from Slovenia, Dana was originally cross bred from the German Hallertau Magnum and native Slovenian varieties. Like any good faux noble hop should, it infuses a rustic, musty spiciness into a toasty beast of a malt base.",
+        "image_url": "https://images.punkapi.com/v2/30.png",
+        "abv": 6.7,
+        "ibu": 70,
+        "food_pairing": [
+            "Spicy Asian-style noodles",
+            "Smokey chipotle beef brisket tostada",
+            "Sweet potato pie"
+        ],
+        
+    },
+    {
+        "id": 31,
+        "name": "Nanny State",
+        "tagline": "Alcohol Free Hoppy Ale.",
+        "first_brewed": "08/2009",
+        "description": "Brewing a full flavoured craft beer at 0.5% is no easy task. Packed with loads of Centennial, Amarillo, Columbus, Cascade and Simcoe hops, dry hopped to the brink and back and sitting at 55 IBUs, Nanny State is a force to be reckoned with. With a backbone of 8 different specialty malts, Nanny State will tantalise your taste buds and leave you yearning for more.",
+        "image_url": "https://images.punkapi.com/v2/31.png",
+        "abv": 0.5,
+        "ibu": 55,
+        
+        "food_pairing": [
+            "Earthy mushroom pasta"
+        ],
+       
+    }
+]
