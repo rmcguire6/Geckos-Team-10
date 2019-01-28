1ffe0dc068c6cf4e1dd10be7808ff2d70c8db2b3
Regina Mc Guire <rmcguire@antelecom.net>
2019-01-28 06:47:45 -0800

    Add content and styling to hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,14 +3,28 @@ import React from 'react';
 const Hero = ({random}) =>{
     const imageURL = random.image_url;
     return(
-        <div className="hero-image">
-            <div className="hero-text">
-                <h1>Beer of The Day</h1>
-                <img src={imageURL} height='260' width='107'/>
-                <figcaption><i>{random.name}</i></figcaption>
-            
+        <div >  
+            <div className="hero-group">
+                <h1 className="colored hero-text">HOPP</h1>
+                <div >
+                    <img className="hero-image" src={imageURL} height='260' width='107'/>
+                </div>               
+                <h1 className="colored hero-text">NESS</h1>
             </div>
-      </div>
+            <div className="hero-group">
+                <div>NO PRESERVATIVES<br/>
+                   NO CHEMICALS<br/>
+                   NO CHEAP SUBSTITUTES<br/>
+                   JUST <span className="bold">BARLEY, HOPS, YEAST, WATER</span><br/>
+                   and <span className="colored big">MIND-BLOWING FLAVOR</span>
+                </div>
+                <div className="circle">
+                   <h3>Featured Beer</h3>
+                   <h4>Get to Know</h4>
+                   <h2>{random.name}</h2>
+                </div>  
+            </div>           
+        </div>
     )
 }
 
