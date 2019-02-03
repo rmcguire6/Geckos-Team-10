a329953cd089f4869e4185c9bdcdefadf5c6d02d
Regina Mc Guire <rmcguire@antelecom.net>
2019-02-03 12:55:00 -0800

    Remove console.log statements in Helpers

--- src/utils/Helpers.js
+++ src/utils/Helpers.js
@@ -22,7 +22,6 @@ const displayUniqueBeers = arr => {
 const filterByValue = (beers ,filterType , filter) => {
   let beerFilter;
   let filteredBeers = [];
-  console.log('filter by Value ', filterType, filter)
   if (filterType === 'abv') {
     beerFilter = 'abv';
   } else if (filterType === 'ibu') {
@@ -36,7 +35,6 @@ const filterByValue = (beers ,filterType , filter) => {
     filteredBeers = beers.filter(beer => (lowerValue <= beer[beerFilter] && beer[beerFilter] < higherValue))
   } else {
     if (filterType === 'text')
-    console.log('text from filter by Value', filterType)
     filteredBeers = filterByType(beers, filterType)
   }
   return filteredBeers;
@@ -48,18 +46,15 @@ const filterByType = (beers , textFilter) => {
       if ((textFilter !== 'ipa') && (textFilter !== 'ale')) {
         filteredBeers = beers.filter(beer => beer.tagline.toLowerCase().includes(textFilter)
           || beer.name.toLowerCase().includes(textFilter))
-        console.log('anything but ale or IPA',textFilter)
       } else if (textFilter === 'ipa') {   
           filteredBeers = beers.filter(beer =>
             beer.tagline.toLowerCase().includes(ipaFilter || textFilter) 
             || beer.name.toLowerCase().includes(ipaFilter || textFilter))
-          console.log('IPA',textFilter)
       } else if (textFilter === 'ale') {
           filteredBeers = beers.filter(beer => beer.tagline.toLowerCase().includes(textFilter) 
              || beer.name.toLowerCase().includes(textFilter))
           filteredBeers = filteredBeers.filter(beer => !(beer.tagline.toLowerCase().includes(ipaFilter) 
             || beer.name.toLowerCase().includes(ipaFilter)))
-            console.log('ale',textFilter)
       }
     }
   return filteredBeers;
@@ -69,15 +64,12 @@ const chooseThisFilteredList = (beers, [abv, ibu, type], filterBy) => {
   if (filterBy  !== '') {
     if (filterBy === 'abv')
     filteredBeers = filterByValue(beers,'abv', abv)
-    console.log('abv', abv, filteredBeers)
   }
     if (filterBy === 'ibu') {
     filteredBeers = filterByValue(beers,'ibu', ibu)
-    console.log('ibu', ibu, filteredBeers)
   }
   if (filterBy ==='type') {
     filteredBeers = filterByType(beers, type)
-    console.log('type', type, filteredBeers)
   }
   return filteredBeers;
 };
